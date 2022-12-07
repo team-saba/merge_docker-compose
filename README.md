78d5c8ec2b3ddd736504e2a613fff4e4ddc3cda0
seungwook0417 <swlee@daegu.ac.kr>
2022-12-08 05:14:47 +0900

    수정

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # merge_docker-compose
 
 ### 프로젝트 파일은 뺐습니다.
-- 대신 스크립트 넣음
+실행순서
+```
+git clone git@github.com:team-saba/merge_docker-compose.git
+cd merge_docker-compose
+chmod 755 ./setup.sh
+./setup.sh
+```
+.env 파일은 다음과 같이 만들어 둡니다.
+```
+# ===== web server setting =====
+HOST=#######
+PORT=######
+COSIGN_PASSWORD='#####'
+COSIGN_DOCKER_MEDIA_TYPES=1
+```
--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
-npm install
-
-git clone https://github.com/team-saba/saba_backend.git && mv saba_backend/* ./back && rm -rf saba_backend
+git clone git@github.com:team-saba/saba_backend.git && mv saba_backend/* ./back && rm -rf saba_backend
 
 cd back && chmod 755 ./entry.sh && cd ..
+mv .env ./back/.env
 
-git clone https://github.com/team-saba/saba_front.git && mv saba_front/* ./front && rm -rf saba_front 
+git clone git@github.com:team-saba/saba_front.git && mv saba_front/* ./front && rm -rf saba_front 
 
-cd front && npm run build && cd ..
+cd front && npm install && npm run build && cd ..
 
 docker-compose up -d
